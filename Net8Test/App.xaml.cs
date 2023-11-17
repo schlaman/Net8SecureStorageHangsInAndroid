601378ec71153b2ea9368936fc32aad32d2d96e6
Craig Schlaman <craig.schlaman@gmail.com>
2023-11-16 16:22:41 -0800

    Add project files.

---
+++ Net8Test/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Net8Test
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            
+            // This line of code never returns - it hangs forever
+            var password = SecureStorage.GetAsync("password").Result;
+
+            MainPage = new AppShell();
+        }
+    }
+}
